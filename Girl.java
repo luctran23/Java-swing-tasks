dbad567fd63576cf6bdd63ebb36ca9e6e7b75258
luctran23 <anhvanluc1999@gmail.com>
2020-05-18 17:07:40 +0700

    Create a girl list form

---
+++ Girl.java
@@ -0,0 +1,21 @@
+
+package Girls;
+
+
+public class Girl {
+    String id;
+    String name;
+    int age;
+    public Girl(){
+        
+    }
+
+    public Girl(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public Girl(String id){
+        this.id = id;
+    }
+}
